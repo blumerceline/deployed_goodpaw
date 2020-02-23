a80cf2aa07f30715ba0fbf22c22f73dd736170a3
blumerceline <blumerceline@gmail.com>
2020-02-23 12:17:29 +1000

    Updates

---
+++ precache-manifest.7ee7ad0286ab1f9c5c7bed2a59e17bc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a98d2493dda9d8c4cd1a760445ae1acc",
+    "url": "/deployed_goodpaw/index.html"
+  },
+  {
+    "revision": "acd62d865a1c3d270a4f",
+    "url": "/deployed_goodpaw/static/css/2.0996cbbd.chunk.css"
+  },
+  {
+    "revision": "3fb98d93f7569b5c658b",
+    "url": "/deployed_goodpaw/static/css/main.34fd7d9a.chunk.css"
+  },
+  {
+    "revision": "acd62d865a1c3d270a4f",
+    "url": "/deployed_goodpaw/static/js/2.5f3255de.chunk.js"
+  },
+  {
+    "revision": "e933f8dbfdf496cb72e495663a499a00",
+    "url": "/deployed_goodpaw/static/js/2.5f3255de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fb98d93f7569b5c658b",
+    "url": "/deployed_goodpaw/static/js/main.ff8d8962.chunk.js"
+  },
+  {
+    "revision": "0ddc5531d6b44ea424b0",
+    "url": "/deployed_goodpaw/static/js/runtime-main.29b718ae.js"
+  }
+]);
